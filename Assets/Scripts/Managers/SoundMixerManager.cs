e6a2fc987b725aa780a7ac8156de81c1d8de88a2
R0drix06 <campregherlautaro@gmail.com>
2025-12-16 20:43:42 -0300

    Fixed - Audio Bug
    Audio error unable to make the build was fixed

--- Assets/Scripts/Managers/SoundMixerManager.cs
+++ Assets/Scripts/Managers/SoundMixerManager.cs
@@ -1,5 +1,3 @@
-using Unity.VisualScripting;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.Audio;
 
@@ -8,28 +6,26 @@ public class AudioMixerManager : MonoBehaviour
     public static AudioMixerManager Instance;
 
     [SerializeField] private AudioMixer audioMixer;
+
     private void Awake()
     {
         if (Instance != null && Instance != this)
         {
-            Destroy(this);
+            Destroy(gameObject);
             return;
         }
-        else
-            Instance = this;
 
-        DontDestroyOnLoad(this);
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
     public void SetMusicVolume(float volume)
     {
         audioMixer.SetFloat("musicVolume", Mathf.Log10(volume) * 20f);
-
     }
 
     public void SetSFXVolume(float volume)
     {
         audioMixer.SetFloat("SFXVolume", Mathf.Log10(volume) * 20f);
-
     }
 }
